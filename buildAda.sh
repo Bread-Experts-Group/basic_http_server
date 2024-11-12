6ab36228bddb815d121339e43b7374c83c84557e
ATPStorages <tcgytsc@gmail.com>
2024-11-11 23:52:19 -0600

    buildAda adoption

--- buildAda.sh
+++ buildAda.sh
@@ -1,2 +1,1 @@
-PROJECT=${2:-${PWD##*/}}
-gnatpp -P$PROJECT.gpr --max-line-length=179 --wide-character-encoding=8 --eol=lf -k --layout=tall -U; alr build --profiles=*=$1; sudo bin/$PROJECT
+gnatpp -Pbasic_http_server.gpr --max-line-length=179 --wide-character-encoding=8 --eol=lf -k --layout=tall -U; alr build --profiles=*=$1; sudo bin/basic_http_server ${@:2}
